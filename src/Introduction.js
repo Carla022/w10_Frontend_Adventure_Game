837a230be7a7d2ac51a7621110b4887c67fe098f
Carla Sofia Moreno Lima <carla@Carlas-MBP.broadband>
2024-05-11 03:01:50 +0100

    adding intro text

--- src/Introduction.js
+++ src/Introduction.js
@@ -6,28 +6,28 @@ const Introduction = () => {
             <h3>A nail-biting fantasy adventure, in a world full of dangerous creatures</h3>
             <div className="intro_section">
             <p>
-                Once, the land and sea flourished under the rule of mighty kings and queens, their realms stretching far and wide, adorned with riches and splendor. But ambition and greed festered within the hearts of rulers, and darkness descended upon the kingdoms.
-                Amidst the chaos, great kingdoms crumbled, consumed by their own tyranny and corruption. Its once-proud banners tattered in the winds, its once-glorious cities lay in ruin, and its people scattered like rice grains.
-
-                In the wake of the fallen kingdom's demise, scattered remnants of its once-proud populace roamed the land, seeking refuge and solace amidst the chaos. Displaced and disheartened, they found themselves drawn to other fractured realms, where whispers of resistance stirred and banners of defiance flew high.
-
-                From the far reaches of the realm, disparate factions and wandering tribes converged, driven by a common purpose—to reclaim their land, their heritage, and their dignity. United by shared loss and bound by a collective longing for justice, they forged alliances born of necessity, forming a mosaic of mini-kingdoms amidst the tumultuous landscape.
-
-                Yet, as they gathered strength and rallied their forces, a darker truth began to emerge. For within the ranks of these newfound allies lurked treachery and strife, fueled by old grievances and festering wounds. Violence became a language spoken too readily, selfishness a guiding principle, and revenge a driving force.
-
-                The once-honorable quest for redemption became tainted by the shadows of ambition and betrayal. Mini-kingdoms vied for supremacy, their leaders blinded by the allure of power and blinded to the cost of their actions. Ancient rivalries resurfaced, blood feuds reignited, and the very fabric of unity threatened to unravel.
-
-                Amidst the clamor of battle and the clash of swords, the line between hero and villain blurred. Those who once fought side by side now stood divided, their noble cause twisted by the corruption of their own hearts.
-
-                As the land trembled beneath the weight of their conflict, a grim realization dawned—while the fallen kingdom may have been vanquished, the greatest threat to its legacy lay not in the armies of its enemies, but in the darkness that lurked within its own ranks.
-
-
-
-
-                Queen Ahris, ruler of The Enen Sea The great knight of The Enen Sea has been summoned to journey through deep forests, deadly mountains and monster infested caves, to defeat the mighty Troll of Tarragon.
-
-           
-            But beware! - You will be entering rival territories...
+                Long ago, the lands and seas flourished under the rule of mighty kings and queens, their realms stretching far and wide, adorned with riches and splendor. 
+                <br/>
+                <br/>
+                But, the once-honorable quest for propesrity birthed a shadow of greed, betrayal and darkness within the hearts of rulers, evil descended upon the kingdoms and wars waged. Amidst the chaos, great kingdoms crumbled, consumed by their own tyranny and corruption. Banners tattered in the winds, their once-glorious cities lay in ruins, and people scattered like rice grains.
+                <br/>
+                <br/>
+                In the wake of the fallen kingdoms demise, remnants of their once-proud populace roamed the land, seeking refuge. They found themselves drawn to other fractured realms and wandering tribes converged, driven by a common purpose — to reclaim their land, their heritage, and their dignity. United by shared loss and bound by a collective longing for justice, they forged alliances born of necessity. Violence became a language spoken too readily, selfishness a guiding principle, and revenge a driving force, and soon whispers of resistance stirred and banners of defiance flew high.
+                <br/>
+                <br/>
+                As the lands began to tremble beneath the weight of these uprisings, a grim realization dawned — factions were no longer seeking reclaimation of their fallen kingdoms...they sought revenge and made foe of anything that stood in their way.
+                <br/>
+                <br/>
+                It is up to the 4 remaining kingdoms of the North and West to come together to eradicate the threat for good.
+                <br/>
+                <br/>
+                Queen Ahris, ruler of The Great Enen Sea, from the North, has summoned the greatest Knight of the Kingdom to journey through deep forests, deadly mountains and monster infested caves, to defeat the mighty Troll of Tarragon.
+                <br/>
+                <br/>
+                <br/>
+                
+                But beware! - You will be entering rival territories...
+            
             </p>
 
             </div>
